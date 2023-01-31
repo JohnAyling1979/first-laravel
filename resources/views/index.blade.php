c066dab1b4b123abecc8f59e2b229b9a1cdddce9
John Ayling <john.ayling@digintent.com>
2023-01-31 14:57:12 -0600

    Add routes and controllers.

---
+++ resources/views/index.blade.php
@@ -0,0 +1,3 @@
+<h1>Home Page [Invoke]</h1>
+
+<a href="{{ route('blog.index') }}">Blogs</a>
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,24 @@
 <?php
 
+use App\Http\Controllers\FallbackController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostsController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+// Route::resource('blog', PostsController::class);
+Route::prefix('/blog')->group(function() {
+    Route::get('/', [PostsController::class, 'index'])->name('blog.index');
+    Route::get('/{id}', [PostsController::class, 'show'])->name('blog.show')->whereNumber('id');
 
-Route::get('/', function () {
-    return view('welcome');
+    Route::get('/create', [PostsController::class, 'create'])->name('blog.create');
+    Route::post('/', [PostsController::class, 'store'])->name('blog.store');
+
+    Route::get('/{id}/edit', [PostsController::class, 'edit'])->name('blog.edit')->whereNumber('id');
+    Route::patch('/{id}', [PostsController::class, 'update'])->name('blog.update')->whereNumber('id');
+
+    Route::delete('/{id}', [PostsController::class, 'destroy'])->name('blog.destroy')->whereNumber('id');
 });
+
+Route::get('/', HomeController::class);
+
+Route::fallback(FallbackController::class);
